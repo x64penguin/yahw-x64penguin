07e492a4031a376b1358a8e1f478f39ec8f096b0
penguin <anton.lobkov.mtb@gmail.com>
2022-11-11 19:54:21 +0300

    все кроме корзины есть

--- src/store/categories/loadCategories.js
+++ src/store/categories/loadCategories.js
@@ -1,15 +1,18 @@
-import { selectCategories } from "./selectors";
+import { selectCategories, selectIsCategoriesLoaded } from "./selectors";
 import { categorySlice } from ".";
 import { prepareData } from "../utils";
 
 export const loadCategories = (dispatch, getState) => {
-      dispatch(categorySlice.actions.startLoading());
-      fetch("http://localhost:3001/api/categories")
-        .then((response) => response.json())
-        .then((categories) => {
-          dispatch(categorySlice.actions.successLoading(prepareData(categories)));
-        })
-        .catch(() => {
-          dispatch(categorySlice.actions.failLoading());
-        });
+  if (selectIsCategoriesLoaded(getState())) {
+    return;
+  }
+  dispatch(categorySlice.actions.startLoading());
+  fetch("http://localhost:3001/api/categories")
+    .then((response) => response.json())
+    .then((categories) => {
+      dispatch(categorySlice.actions.successLoading(prepareData(categories)));
+    })
+    .catch(() => {
+      dispatch(categorySlice.actions.failLoading());
+    });
 }
--- src/store/reviews/loadReview.js
+++ src/store/reviews/loadReview.js
@@ -1,15 +1,19 @@
 import { reviewSlice } from ".";
 import { prepareData } from "../utils";
-import { selectReviews } from "./selectors";
+import { selectReviews, selectReviewsIsLoaded } from "./selectors";
 
-export const loadReviews = (dispatch) => {
-      dispatch(reviewSlice.actions.startLoading());
-      fetch("http://localhost:3001/api/reviews")
-        .then((response) => response.json())
-        .then((reviews) => {
-          dispatch(reviewSlice.actions.successLoading(prepareData(reviews)));
-        })
-        .catch(() => {
-          dispatch(reviewSlice.actions.failLoading());
-        });
+export const loadReviews = (dispatch, getState) => {
+  if (selectReviewsIsLoaded(getState())) {
+    return;
+  }
+
+  dispatch(reviewSlice.actions.startLoading());
+  fetch("http://localhost:3001/api/reviews")
+    .then((response) => response.json())
+    .then((reviews) => {
+      dispatch(reviewSlice.actions.successLoading(prepareData(reviews)));
+    })
+    .catch(() => {
+      dispatch(reviewSlice.actions.failLoading());
+    });
 }
